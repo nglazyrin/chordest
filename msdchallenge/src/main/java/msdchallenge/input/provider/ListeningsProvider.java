0fd78cdc8515c6bf2e1bb034080c1c4f60cbf4c5
nglazyrin <nglazyrin@gmail.com>
2012-08-02 02:21:46 +0600

    Typo fix

--- msdchallenge/src/main/java/msdchallenge/input/provider/ListeningsProvider.java
+++ msdchallenge/src/main/java/msdchallenge/input/provider/ListeningsProvider.java
@@ -15,9 +15,9 @@ public class ListeningsProvider {
 
 	public ListeningsProvider() {
 		tracks = IoUtil.deserialize(Constants.TRACKS_BY_USER_FILE);
-		LOG.info(tracks.length + "users' listenings have been read from " + Constants.TRACKS_BY_USER_FILE);
+		LOG.info(tracks.length + " users' listenings have been read from " + Constants.TRACKS_BY_USER_FILE);
 		counts = IoUtil.deserialize(Constants.COUNTS_BY_USER_FILE);
-		LOG.info(tracks.length + "users' listening counts have been read from " + Constants.COUNTS_BY_USER_FILE);
+		LOG.info(tracks.length + " users' listening counts have been read from " + Constants.COUNTS_BY_USER_FILE);
 	}
 
 	public int[][] getTracks() {
