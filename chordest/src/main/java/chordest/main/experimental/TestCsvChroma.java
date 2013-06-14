84cc0e9579529831b7931c2000fd80d8e9700fd5
nglazyrin <nglazyrin@gmail.com>
2013-06-14 17:00:43 +0600

    Option to use CRP features together with ones obtained from neural network.

--- chordest/src/main/java/chordest/main/experimental/TestCsvChroma.java
+++ chordest/src/main/java/chordest/main/experimental/TestCsvChroma.java
@@ -4,14 +4,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import chordest.chord.Harmony;
 import chordest.chord.recognition.TemplatesRecognition;
 import chordest.chord.templates.ITemplateProducer;
 import chordest.chord.templates.TemplateProducer;
 import chordest.configuration.Configuration;
 import chordest.io.csv.CsvSpectrumFileReader;
+import chordest.io.spectrum.SpectrumFileReader;
+import chordest.main.VisualDebugger;
 import chordest.model.Chord;
 import chordest.model.Note;
+import chordest.spectrum.SpectrumData;
+import chordest.transform.DiscreteCosineTransform;
 import chordest.transform.ScaleInfo;
 import chordest.util.DataUtil;
 import chordest.util.PathConstants;
@@ -36,37 +42,69 @@ public class TestCsvChroma extends AbstractTestRecognizeFromCsv {
 	public Chord[] recognize(File csvFile) {
 		double[][] chroma = new CsvSpectrumFileReader(csvFile).getSpectrum();
 		
-		List<Integer> noChords = new ArrayList<Integer>();
+		double[] noChordness = new double[chroma.length];
 		for (int i = 0; i < chroma.length; i++) {
 			double[] t = chroma[i];
 			double max = 0;
 			for (int j = 0; j < t.length; j++) {
 				max = Math.max(Math.abs(t[j]), max);
 			}
-			if (max < 4.5) {
-				noChords.add(i);
+			if (max < 1.5) { // TODO: was 4.5
+				noChordness[i] = 0;
+			} else {
+				noChordness[i] = 1;
 			}
 		}
-		DataUtil.scaleEachTo01(chroma);
-		double[][] selfSim = DataUtil.getSelfSimilarity(chroma);
-		selfSim = DataUtil.removeDissimilar(selfSim, 0.1);
-		chroma = DataUtil.smoothWithSelfSimilarity(chroma, selfSim);
 		
-//		String[] labels = NoteLabelProvider.getNoteLabels(c.spectrum.offsetFromF0InSemitones, new ScaleInfo(1, 12));
-//		double[] beatTimes = new double[chroma.length];
-//		for (int i = 0; i < beatTimes.length; i++) { beatTimes[i] = i; }
-//		Visualizer.visualizeSpectrum(chroma, beatTimes, labels, "Chroma");
+//		int octaves = 4;
+//		double[][] binSpectrum = getBinSpectrum(csvFile, octaves);
+//		noChordness = DataUtil.getNochordness(binSpectrum, octaves);
+//		double[][] chroma2 = getBinChroma(binSpectrum, octaves);
+//		chroma = chroma2;
+//		for (int i = 0; i < chroma.length; i++) {
+//			chroma[i] = DataUtil.add(chroma[i], chroma2[i]);
+//		}
+		
+		DataUtil.scaleEachTo01(chroma);
+		
+		double[][] selfSim = DataUtil.getSelfSimilarity(chroma);
+		selfSim = DataUtil.removeDissimilar(selfSim, c.process.selfSimilarityTheta);
+		chroma = DataUtil.smoothWithSelfSimilarity(chroma, selfSim);
 		
 		Note startNote = Note.byNumber(c.spectrum.offsetFromF0InSemitones);
 		ITemplateProducer producer = new TemplateProducer(startNote);
 		TemplatesRecognition rec = new TemplatesRecognition(producer);
 		Chord[] temp = rec.recognize(chroma, new ScaleInfo(1, 12));
-		for (Integer idx : noChords) {
-			temp[idx] = Chord.empty();
+		for (int i = 0; i < noChordness.length; i++) {
+			if (noChordness[i] < 0.0015) {
+				temp[i] = Chord.empty();
+			}
 		}
 		return Harmony.smoothUsingHarmony(chroma, temp, new ScaleInfo(1, 12), producer);
 //		return new Viterbi(producer).decode(chroma);
 //		return temp;
+	}
+
+	private double[][] getBinChroma(double[][] spectrum, int octaves) {
+		double[][] chroma = DataUtil.reduce(spectrum, octaves);
+		chroma =  DataUtil.toSingleOctave(chroma, 12);
+		DataUtil.scaleEachTo01(chroma);
+		return chroma;
+	}
+
+	private double[][] getBinSpectrum(File csvFile, int octaves) {
+		String binFileName = StringUtils.substringBeforeLast(csvFile.getName(), PathConstants.EXT_CSV);
+		SpectrumData sd = SpectrumFileReader.read("spectrum8-60-6\\" + binFileName);
+		VisualDebugger.getOctaves(sd, 0, octaves);
+		double[][] spectrum = sd.spectrum;
+		spectrum = DataUtil.smoothHorizontallyMedian(spectrum, c.process.medianFilterWindow);
+		spectrum = DataUtil.shrink(spectrum, sd.framesPerBeat);
+		spectrum = DataUtil.toLogSpectrum(spectrum);
+		spectrum = DiscreteCosineTransform.doChromaReduction(spectrum, c.process.crpFirstNonZero);
+		double[][] selfSim = DataUtil.getSelfSimilarity(spectrum);
+		selfSim = DataUtil.removeDissimilar(selfSim, c.process.selfSimilarityTheta);
+		spectrum = DataUtil.smoothWithSelfSimilarity(spectrum, selfSim);
+		return spectrum;
 	}
 
 }
